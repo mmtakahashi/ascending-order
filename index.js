cad781d8efb0da26695b77ea41ae1fe1ce19b072
mmtakahashi <m.takahashi@gftd.works>
2022-05-25 15:29:46 +0900

    ADD : comment

--- index.js
+++ index.js
@@ -7,53 +7,61 @@ let myHistory = [];
 
 const onClickadd = () => {
     if(inputOrder.value !== ''){
+        // 配列の最初に要素を追加していく
         myHistory.unshift(inputOrder.value);
+        // 要素が入力されたら入力欄を空にする
         inputOrder.value = '';
         
+        // 親要素の最初の子要素（firstChild）がなくなるまで、removeChildで削除し続ける
         while(inputArea.firstChild){
             inputArea.removeChild(inputArea.firstChild);
         }
 
+        // 全角英数を半角英数にする
         const toHalfWidth = str => {
             return str.replace(/[\uFF01-\uFF5E]/g, ch => {
               return String.fromCharCode(ch.charCodeAt(0) - 0xFEE0);
             });
         };
         
+        // liを生成して配列を表示する
         for(let i = 0; i < myHistory.length; i++) {
-            const itemText = myHistory[i];
+            const itemText = toHalfWidth(myHistory[i]);
             const listItem = document.createElement('li');
-            const half = toHalfWidth(itemText);
-            listItem.textContent = half;
+            listItem.textContent = itemText;
             inputArea.appendChild(listItem);
         }
-    } 
+    }
+    // 入力欄にフォーカスを当てる 
     inputOrder.focus();   
 }
 
 const updateList = asc => {
+    // 親要素の最初の子要素（firstChild）がなくなるまで、removeChildで削除し続ける
     while(inputArea.firstChild){
         inputArea.removeChild(inputArea.firstChild);
     }
 
+    // 全角英数を半角英数にする
     const toHalfWidth = str => {
         return str.replace(/[\uFF01-\uFF5E]/g, ch => {
           return String.fromCharCode(ch.charCodeAt(0) - 0xFEE0);
         });
     };
 
-    const toHalf = half => toHalfWidth(half);
-    
+    // liを生成して昇順になった配列を表示する
     for(let i = 0; i < asc.length; i++) {
-        const ascText = toHalf(asc[i]);
+        const ascText = toHalfWidth(asc[i]);
         const listItem = document.createElement('li');
         listItem.textContent = ascText;
+        console.log(ascText);
         inputArea.appendChild(listItem);
     }
 }
 
+// 昇順ボタンが押されたらupdateListにasceding関数を送る
 const onClickasc = () => {
-    let ascending = myHistory.sort((a,b)=>{
+    const ascending = myHistory.sort((a,b)=>{
         return a - b;
     })
     updateList(ascending);
